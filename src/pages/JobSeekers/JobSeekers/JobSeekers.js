70a9bfb23b73861ea8e5edbaff429d806871d55b
MdHasanRaju <mdhasanraju2017@gmail.com>
2022-07-10 21:13:41 +0600

    seekers testimonial added

--- src/pages/JobSeekers/JobSeekers/JobSeekers.js
+++ src/pages/JobSeekers/JobSeekers/JobSeekers.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import ScanApp from '../../Shared/ScanApp/ScanApp';
 import JobSeekersAll from '../JobSeekersAll/JobSeekersAll'; 
+import SeekersTestimonial from '../SeekersTestimonial/SeekersTestimonial';
 
 const JobSeekers = () => {
     return (
         <div>
             <JobSeekersAll/>
+            <SeekersTestimonial/>
             <ScanApp/>
             <Footer/>
         </div>
---
+++ src/pages/JobSeekers/SeekersTestimonial/SeekersTestimonial.js
@@ -0,0 +1,197 @@
+import React from "react";
+import "./SeekersTestimonial.css";
+import palak from "../../../assets/images/palak-sharma.2ab3146.png";
+import pratyush from "../../../assets/images/pratyush.ebff6dd.png";
+import rudra from "../../../assets/images/rudra.5da3717.png";
+import ayush from "../../../assets/images/ayush.4f54877.png";
+
+const SeekersTestimonial = () => {
+  return (
+    <section className="seeker-testimonial">
+      <div className="container">
+        <h3>
+          Job Seekers Love Hirect, <br />
+          Here’s Why?
+        </h3>
+        <div className="row responsive">
+          <div className="col-lg-12">
+            <div
+              tabindex="-1"
+              data-index="-3"
+              aria-hidden="true"
+              class="slick-slide slick-cloned"
+              style={{ width: "372px" }}
+            >
+              <div>
+                <div
+                  tabindex="-1"
+                  class="testimonial-card"
+                  style={{ width: "100%", display: "inline-block" }}
+                >
+                  <div class="seeker-img">
+                    <img src={palak} alt="" />
+                  </div>{" "}
+                  <h4>Palak Sharma</h4>{" "}
+                  <p>
+                    There are many job opportunities available on Hirect. I was
+                    surfing through this app and found that it is helpful for
+                    freshers, especially those searching for jobs.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-12">
+            <div
+              tabindex="-1"
+              data-index="-3"
+              aria-hidden="true"
+              class="slick-slide slick-cloned"
+              style={{ width: "372px" }}
+            >
+              <div>
+                <div
+                  tabindex="-1"
+                  class="testimonial-card"
+                  style={{ width: "100%", display: "inline-block" }}
+                >
+                  <div class="seeker-img">
+                    <img src={pratyush} alt="" />
+                  </div>{" "}
+                  <h4>Pratyush R.</h4>{" "}
+                  <p>
+                    Now, it takes just a fraction of a second to find a
+                    respectful and relevant job. Thanks to the Hirect App,
+                    things are much more convenient and simplified for millions
+                    of job seekers. I always recommend this App to my friends
+                    because I believe it's better than other job seeking apps,
+                    I've come across till now.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-12">
+            <div
+              tabindex="-1"
+              data-index="-3"
+              aria-hidden="true"
+              class="slick-slide slick-cloned"
+              style={{ width: "372px" }}
+            >
+              <div>
+                <div
+                  tabindex="-1"
+                  class="testimonial-card"
+                  style={{ width: "100%", display: "inline-block" }}
+                >
+                  <div class="seeker-img">
+                    <img src={rudra} alt="" />
+                  </div>{" "}
+                  <h4>Rudraksh Parihar</h4>{" "}
+                  <p>
+                    Are you looking for a job? Or are you looking to hire a
+                    team? Hirect makes it easier for you. You can directly chat
+                    with recruiters or with job seekers. How wonderful is that,
+                    isn't it? All the recruiters are verified, and it is free.
+                    Hire or get hired within a week. It is available on the
+                    Google play store and the app store.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-12">
+            <div
+              tabindex="-1"
+              data-index="-3"
+              aria-hidden="true"
+              class="slick-slide slick-cloned"
+              style={{ width: "372px" }}
+            >
+              <div>
+                <div
+                  tabindex="-1"
+                  class="testimonial-card"
+                  style={{ width: "100%", display: "inline-block" }}
+                >
+                  <div class="seeker-img">
+                    <img src={ayush} alt="" />
+                  </div>{" "}
+                  <h4>Ayush Parmar</h4>{" "}
+                  <p>
+                    Hirect is an excellent app for searching for jobs, and I
+                    also love the options provided on the platform for the Jobs.
+                    There are many opportunities available for Technical Jobs.
+                    My favorite aspect of this website is that there are no
+                    charges for posting jobs and you can contact the Human
+                    Resources and Hiring Managers directly.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-12">
+            <div
+              tabindex="-1"
+              data-index="-3"
+              aria-hidden="true"
+              class="slick-slide slick-cloned"
+              style={{ width: "372px" }}
+            >
+              <div>
+                <div
+                  tabindex="-1"
+                  class="testimonial-card"
+                  style={{ width: "100%", display: "inline-block" }}
+                >
+                  <div class="seeker-img">
+                    <img src={palak} alt="" />
+                  </div>{" "}
+                  <h4>Palak Sharma</h4>{" "}
+                  <p>
+                    There are many job opportunities available on Hirect. I was
+                    surfing through this app and found that it is helpful for
+                    freshers, especially those searching for jobs.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-12">
+            <div
+              tabindex="-1"
+              data-index="-3"
+              aria-hidden="true"
+              class="slick-slide slick-cloned"
+              style={{ width: "372px" }}
+            >
+              <div>
+                <div
+                  tabindex="-1"
+                  class="testimonial-card"
+                  style={{ width: "100%", display: "inline-block" }}
+                >
+                  <div class="seeker-img">
+                    <img src={pratyush} alt="" />
+                  </div>{" "}
+                  <h4>Pratyush R.</h4>{" "}
+                  <p>
+                    Now, it takes just a fraction of a second to find a
+                    respectful and relevant job. Thanks to the Hirect App,
+                    things are much more convenient and simplified for millions
+                    of job seekers. I always recommend this App to my friends
+                    because I believe it's better than other job seeking apps,
+                    I've come across till now.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SeekersTestimonial;
